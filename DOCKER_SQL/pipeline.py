11ac963dcf8c2e35a337f8b69da84039747b478a
Federico Boccazzi <84158091+fede19bocc@users.noreply.github.com>
2025-01-14 23:47:57 +0000

    principio de pipeline y dockerfile

---
+++ DOCKER_SQL/pipeline.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+
+print(sys.argv) #argumentos que le paso al archivo pipeline desde la terminal
+
+dia = sys.argv[1]
+
+# hago algo con pandas
+
+print('trabajo finalizado de forma exitosa para el dia = {dia}')
